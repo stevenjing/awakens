53a5248b724c4293db7282a9d0f9e51eac6110d1
Marvin Corro <corro.marvin@gmail.com>
2017-09-23 16:38:27 -0700

    added Table

---
+++ client/components/Table.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {Grid, Row, Col, Jumbotron} from 'react-bootstrap';
+import "../styles/commonStyles.scss"
+
+export default class Table extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <Grid>
+            <Row>
+                <Col sm={12} md={12} lg={12}>
+                    <Jumbotron>
+                        <h1>Bootstrap test</h1>
+                    </Jumbotron>
+                </Col>
+            </Row>
+        </Grid>
+    </div>);
+  }
+}
